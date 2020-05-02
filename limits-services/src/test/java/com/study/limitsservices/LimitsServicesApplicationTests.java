79bd807b016913e356f3346ba1688b300052c8ab
vinaymachinetest <developer.vinay.28@gmail.com>
2020-05-02 23:13:02 +0530

    initial commit for limit services

---
+++ limits-services/src/test/java/com/study/limitsservices/LimitsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.limitsservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
