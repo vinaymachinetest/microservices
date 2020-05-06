a9540ad617c639f720f6636b015414a8cdc97ac8
vinaymachinetest <developer.vinay.28@gmail.com>
2020-05-07 00:11:52 +0530

    enabled ribbon

--- currency-conversion/src/main/java/com/study/services/CurrenyExchangeServiceProxy.java
+++ currency-conversion/src/main/java/com/study/services/CurrenyExchangeServiceProxy.java
@@ -1,10 +1,13 @@
 package com.study.services;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="currency-exchage-service",  url="localhost:8000")
+//@FeignClient(name="currency-exchage-service",  url="localhost:8000")	
+@FeignClient(name="currency-exchage-service")	
+@RibbonClient(name="currency-exchage-service")
 public interface CurrenyExchangeServiceProxy {
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
